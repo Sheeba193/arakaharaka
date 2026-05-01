8e3c098caf50d5f7aa6dab13a572fc128d2bed05
Sheeba193 <bethkerubo193@gmail.com>
2026-05-02 02:53:51 +0300

    html structure

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer text-center py-6">
-      <p>© {new Date().getFullYear()} Arakaharaka Enterprise</p>
-      <p>Your Trusted PI Merchant</p>
-    </footer>
-  );
-}
